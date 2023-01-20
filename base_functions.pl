aa92fa8ea08e232d7a2b8d7daee0e06b93f3427a
LorenzoLongarini <65166137+LorenzoLongarini@users.noreply.github.com>
2023-01-20 18:05:43 +0100

    Delete base_functions.pl

--- base_functions.pl
+++
@@ -1,9 +0,0 @@
-take_scorers(X,Y):-marcatore(X,Y,_,_).
-take_assistmen(X,W):-marcatore(X,_,W,_).
-take_penalties(X,Z):-marcatore(X,_,_,Z).
-
-count([],0).
-count([1|T],N) :- count(T,N1), N is N1 + 1.
-
-winner_home(X,Result):-findall(X,matchSA(X,_,"HOME_TEAM",_,_),Result).
-num_winner_home(X,Count):-winner_home(X,Result),length(Result,Count).
