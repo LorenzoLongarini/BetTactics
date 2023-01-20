db134c035c8724a9de3e98a695be318b71a4e6f6
LorenzoLongarini <65166137+LorenzoLongarini@users.noreply.github.com>
2023-01-20 18:06:01 +0100

    Add files via upload

---
+++ base_functions.pl
@@ -0,0 +1,12 @@
+take_scorers(X,Y):-marcatore(X,Y,_,_).
+take_assistmen(X,W):-marcatore(X,_,W,_).
+take_penalties(X,Z):-marcatore(X,_,_,Z).
+
+count([],0).
+count([1|T],N) :- count(T,N1), N is N1 + 1.
+
+winner_home(X,Result):-findall(X,matchSA(X,_,"HOME_TEAM",_,_),Result).
+num_winner_home(X,Count):-winner_home(X,Result),length(Result,Count).
+
+winner_away(X,Result):-findall(X,matchSA(_,X,"AWAY_TEAM",_,_),Result).
+num_winner_away(X,Count):-winner_away(X,Result),length(Result,Count).
