5b608da9635bf7acd0a7befc6e69bc43f3391253
LorenzoLongarini <s1089145@studenti.univpm.it>
2023-02-02 16:52:53 +0100

    ai

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+    preload: path.join(__dirname, 'preload.js'),
+    }, 
+  })
+  //ipcMain.hande('ping', () => 'pong')
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
