4ca3de39cfc425b95d0ac681edf5b70bef470d34
Loris Ramovini <loris.ramovini@outlook.com>
2023-02-07 12:05:30 +0100

    scaricamento db squadra

---
+++ prolog/database_RESULTS_SA.pl
@@ -0,0 +1,20 @@
+classifica(1,"SSC Napoli", 113, "W,W,W,W,W").
+classifica(2,"FC Internazionale Milano", 108, "W,W,L,W,D").
+classifica(3,"AS Roma", 100, "W,L,W,W,D").
+classifica(4,"SS Lazio", 110, "D,D,W,W,D").
+classifica(5,"Atalanta BC", 102, "L,W,D,W,W").
+classifica(6,"Juventus FC", 109, "L,D,L,W,W").
+classifica(7,"AC Milan", 98, "L,L,L,D,D").
+classifica(8,"Torino FC", 586, "W,D,W,L,D").
+classifica(9,"Udinese Calcio", 115, "L,D,W,L,L").
+classifica(10,"Bologna FC 1909", 103, "W,W,D,W,L").
+classifica(11,"AC Monza", 5911, "D,W,D,W,D").
+classifica(12,"Empoli FC", 445, "L,D,W,W,D").
+classifica(13,"ACF Fiorentina", 99, "L,D,L,L,W").
+classifica(14,"US Lecce", 5890, "W,L,L,D,D").
+classifica(15,"US Sassuolo Calcio", 471, "W,W,D,L,L").
+classifica(16,"US Salernitana 1919", 455, "W,L,L,D,L").
+classifica(17,"Spezia Calcio", 488, "L,L,L,W,D").
+classifica(18,"Hellas Verona FC", 450, "D,D,W,L,W").
+classifica(19,"UC Sampdoria", 584, "D,L,L,L,L").
+classifica(20,"US Cremonese", 457, "L,L,D,L,L").
